16756ff957fc06c954c1f8dfbe1021df77251a4c
john-zhang1 <john-zhang1@hotmail.com>
2019-04-08 11:29:35 -0500

    initial check in

---
+++ dspace-jspui/src/main/java/org/dspace/app/webui/util/LinkDisplayStrategy.java
@@ -0,0 +1,34 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.webui.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+
+import org.apache.log4j.Logger;
+import org.dspace.content.Metadatum;
+import org.dspace.core.Utils;
+
+public class LinkDisplayStrategy extends AUniformDisplayStrategy
+{
+    /** log4j category */
+    private static Logger log = Logger.getLogger(LinkDisplayStrategy.class);
+
+    protected String getDisplayForValue(HttpServletRequest hrq, String value, int itemid)
+    {
+        StringBuffer sb = new StringBuffer();
+        String startLink = "<a href=\"" + value + "\">";
+        String endLink = "</a>";
+        sb.append(startLink);
+        sb.append(value);
+        sb.append(endLink);
+        return sb.toString();
+    }
+
+}
